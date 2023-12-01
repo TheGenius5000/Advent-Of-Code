ddb9a4ed91c1e026a5ea69230e61fac4b0c6645b
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-01 13:32:12 +0000

    day 1 part 2

---
+++ 2023/Day 1/day1_2.py
@@ -0,0 +1,39 @@
+import numpy as np
+import time
+
+calibration_vals = []
+numbers = {
+    'one': '1',
+    'two': '2', 
+    'three': '3', 
+    'four': '4',
+    'five': '5', 
+    'six': '6', 
+    'seven': '7', 
+    'eight': '8', 
+    'nine': '9'
+}
+
+def wordnum(x, from_beginning=True):
+    max = len(x)
+    size = 1
+    while(size <= max):
+        string = x[:size] if from_beginning else x[-size:]
+        for k, v in numbers.items():
+            if k in string or v in string:
+                return v  
+        size += 1
+
+start = time.perf_counter()
+with open("input.txt", "r") as f:
+    for line in f:
+        first = wordnum(line)
+        last = wordnum(line, False)
+        calibrated_num = first+last
+        calibration_vals.append(int(calibrated_num))
+        #print(first, last)
+
+total = np.sum(calibration_vals)
+end = time.perf_counter()
+print(total)
+print(end-start)
