7a2286c3c4fa83810fb6624fa36e06f82aa1273c
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-04 12:07:09 +0000

    context free input

--- 2023/Day 04/day4_2.py
+++ 2023/Day 04/day4_2.py
@@ -5,7 +5,7 @@ wins = []
 total = 0
 
 start = time.perf_counter()
-with open("D:/GitHub/Advent-Of-Code/2023/Day 04/input.txt") as f:
+with open("input.txt") as f:
   wins = list(map(lambda x: (len([n for n in x[0] if n in x[1]])),list(map(lambda x: list(map(lambda y: y.split(), x)) ,list(map(lambda x: x.split(":")[1].split("|"), list(map(lambda x: x.strip("/n"), f.readlines()))))))))
 
 wins = np.array(wins)
