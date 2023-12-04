7beadf4b24734bfd5fd9fb770a18958619b2f88e
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-04 10:42:50 +0000

    Day 4 part 1

---
+++ 2023/Day 04/day4_1.py
@@ -0,0 +1,5 @@
+import time
+start = time.perf_counter()
+print(sum(list(map(lambda x: 0 if x < 1 else 2**(x-1), list(map(lambda x: (len([n0 for n0 in x[0] if n0 in x[1]])),list(map(lambda x: list(map(lambda y: y.split(), x)) ,list(map(lambda x: x.split(":")[1].split("|"), list(map(lambda x: x.strip("\n"), open("D:/GitHub/Advent-Of-Code/2023/Day 04/input.txt").readlines()))))))))))))
+end = time.perf_counter()
+print(end-start)
