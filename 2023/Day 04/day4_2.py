075ffd99be7af2879783ca1da9803b44ae99c267
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-04 12:05:56 +0000

    optimisation

--- 2023/Day 04/day4_2.py
+++ 2023/Day 04/day4_2.py
@@ -5,15 +5,15 @@ wins = []
 total = 0
 
 start = time.perf_counter()
-with open("input.txt") as f:
-  wins = list(map(lambda x: (len([n for n in x[0] if n in x[1]])),list(map(lambda x: list(map(lambda y: y.split(), x)) ,list(map(lambda x: x.split(":")[1].split("|"), list(map(lambda x: x.strip("\n"), f.readlines()))))))))
+with open("D:/GitHub/Advent-Of-Code/2023/Day 04/input.txt") as f:
+  wins = list(map(lambda x: (len([n for n in x[0] if n in x[1]])),list(map(lambda x: list(map(lambda y: y.split(), x)) ,list(map(lambda x: x.split(":")[1].split("|"), list(map(lambda x: x.strip("/n"), f.readlines()))))))))
 
+wins = np.array(wins)
 scratchcard_instances = np.ones((len(wins)), dtype=int)
 
 for i, win in enumerate(wins):
-  total += scratchcard_instances[i]
   scratchcard_instances[i+1:i+win+1] += scratchcard_instances[i]
 
-print(total)
+print(np.sum(scratchcard_instances))
 end = time.perf_counter()
 print(end-start)
