df8b7f99da1e22bb1486dd0f6d714b4d5840114f
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-08 15:24:10 +0000

    day 8 part 1

---
+++ 2023/Day 08/day8_1.py
@@ -0,0 +1,34 @@
+import time
+
+start = time.perf_counter()
+with open("D:/GitHub/Advent-Of-Code/2023/Day 08/input.txt") as f:
+    lines = f.readlines()
+
+directions = lines[0].strip()
+lines = lines[2:]
+
+#Key: What the location is
+#Value: tuple of locations (left, right)
+camel_map = {}
+
+for line in lines:
+  locations = ''.join(filter(lambda x: x.isalpha() or x.isspace(), line)).split()
+  camel_map.update({locations[0]: (locations[1], locations[2])})
+
+def zzz_finder():
+  curr_location = "AAA"
+  iterations = 0
+  while True:
+    for ch in directions:
+      if curr_location == "ZZZ": return iterations
+      iterations += 1
+      options = camel_map.get(curr_location)
+      if ch == "L":
+        curr_location = options[0]
+      elif ch == "R":
+        curr_location = options[1]
+
+print(zzz_finder())
+
+end = time.perf_counter()
+print(end-start)
