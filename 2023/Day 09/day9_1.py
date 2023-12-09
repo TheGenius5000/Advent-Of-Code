484c09791b9115be480a7b6ed3427704d251850d
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-09 15:06:31 +0000

    Day 9

---
+++ 2023/Day 09/day9_1.py
@@ -0,0 +1,28 @@
+import time
+
+def diff(nums):
+  difference = []
+  last_diff = 0
+  for i, n in enumerate(nums[:-1]):
+    diff = n - nums[i+1]
+    difference.append(diff)
+    last_diff = diff
+  return difference
+
+start = time.perf_counter()
+extrapolations = []
+
+with open("input.txt") as f:
+  for line in f:
+    nums = list(map(int, line.strip().split()))
+    differences = diff(nums)
+    difference_next = [differences[0]]
+    while (len(differences) > 1):
+      differences = diff(differences)
+      difference_next.append(differences[0])
+    extrapolations.append(nums[0] + sum(difference_next))
+
+print(sum(extrapolations))
+
+end = time.perf_counter()
+print(end-start)
