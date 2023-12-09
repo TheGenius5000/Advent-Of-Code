2362d8277037a1c24a3cdc31727986c2f44e97f6
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-09 15:12:36 +0000

    day 9 faster

--- 2023/Day 09/day9_1.py
+++ 2023/Day 09/day9_1.py
@@ -14,10 +14,10 @@ extrapolations = []
 
 with open("input.txt") as f:
   for line in f:
-    nums = list(map(int, line.strip().split()))
+    nums = list(map(int, line.strip().split()[::-1]))
     differences = diff(nums)
     difference_next = [differences[0]]
-    while (len(differences) > 1):
+    while (any(differences)):
       differences = diff(differences)
       difference_next.append(differences[0])
     extrapolations.append(nums[0] + sum(difference_next))
