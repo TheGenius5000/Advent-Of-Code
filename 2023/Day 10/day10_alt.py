f33ba738a29de4204fd42c53ddab71342535ab9d
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-10 16:39:37 +0000

    Geometric answer to day 10

---
+++ 2023/Day 10/day10_alt.py
@@ -0,0 +1,66 @@
+import time
+import numpy as np
+
+def path_finder(place, orientation):
+  length = 0
+  x, y = place
+  char = lines[x][y]
+  space[x][y] = char
+  while True:
+    #if char == "S": break
+    match orientation:
+      case "S":
+        place = (x+1, y)
+      case "N":
+        place = (x-1, y)
+      case "W":
+        place = (x, y-1)
+      case "E":
+        place = (x, y+1)
+    x, y = place
+    char = lines[x][y]
+    space[x][y] = char
+    if char in "F7JL":
+      x_coords.append(x)
+      y_coords.append(y)
+    length += 1
+    match char:
+      case "S":
+        break
+      case "L":
+        orientation = "N" if orientation == "W" else "E"
+      case "F":
+        orientation = "S" if orientation == "W" else "E"
+      case "J":
+        orientation = "N" if orientation == "E" else "W"
+      case "7":
+        orientation = "S" if orientation == "E" else "W"
+  return length
+
+start = time.perf_counter()
+
+with open("D:/GitHub/Advent-Of-Code/2023/Day 10/input.txt") as f:
+  lines = [x.strip() for x in f.readlines()]
+
+start_location = [(i, x.index("S")) for i, x in enumerate(lines) if "S" in x][0]
+
+width = len(lines[0])
+space = [list(''.ljust(width)) for x in lines]
+x_coords = []
+y_coords = []
+
+#You're gonna have to give me the direction of the pipe for start here...
+length = path_finder(start_location, "W")
+
+
+#Shoelace formula
+s = np.roll(y_coords, 1)
+A = 0.5*np.abs(np.dot(x_coords, np.roll(y_coords, 1)) - np.dot(y_coords, np.roll(x_coords, 1)))
+print(A, len(x_coords))
+#Pick's theorem
+i = A+1-length/2
+
+print(length//2)
+print(i)
+end = time.perf_counter()
+print(end-start)
