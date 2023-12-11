78aeaa4e10cfc5cd8729a8d2644d12c099c4dcaa
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-11 19:01:26 +0000

    Day 11 cleanup

--- 2023/Day 11/Day11.py
+++ 2023/Day 11/Day11.py
@@ -1,21 +1,13 @@
 import time
 start = time.perf_counter()
-universe_small = [x.strip() for x in open("D:/GitHub/Advent-Of-Code/2023/Day 11/input.txt").readlines()]
-empty_columns = [j for j in range(len(universe_small[0])) if '#' not in [linecheck[j] for linecheck in universe_small]]
-""" for line in universe_small:
-  for i, j in enumerate(empty_columns):
-    line = line[:j+2*i]
- """
-empty_rows = [i for i, line in enumerate(universe_small) if '#' not in line]
+universe = [x.strip() for x in open("input.txt").readlines()]
+empty_columns = [j for j in range(len(universe[0])) if '#' not in [linecheck[j] for linecheck in universe]]
+empty_rows = [i for i, line in enumerate(universe) if '#' not in line]
 
-#universe_smallrows = [''.join(['.'*1000000 if i in empty_columns else c for i, c in enumerate(line)]) for line in universe_small]
-#universe = sum([[line for x in range(1000000)] if '#' not in line else [line] for i, line in enumerate(universe_smallrows) ], [])
-#[print(''.join(x)) for x in universe]
-galaxy_coords = sum([[(i, j) for j, x in enumerate(line) if x == '#'] for (i, line) in enumerate(universe_small)], [])
+galaxy_coords = sum([[(i, j) for j, x in enumerate(line) if x == '#'] for (i, line) in enumerate(universe)], [])
 end = time.perf_counter()
 print(end-start)
 total = 0
-
 
 for i, (a, b) in enumerate(galaxy_coords):
   for j, (c, d) in enumerate(galaxy_coords[i+1:]):
@@ -56,10 +48,10 @@ print(total)
     sum(
       [1000000-1 for x in 
         [i for i, line in enumerate(
-          [x.strip() for x in open("D:/GitHub/Advent-Of-Code/2023/Day 11/input.txt").readlines()]) 
+          [x.strip() for x in open("input.txt").readlines()]) 
         if '#' not in line] 
       if x in range(min(c,a), max(c,a))]
       +[1000000-1 for y in 
         [j for j in range(10) if '#' not in 
-         [linecheck[j] for linecheck in [x.strip() for x in open("D:/GitHub/Advent-Of-Code/2023/Day 11/input.txt").readlines()]]] 
+         [linecheck[j] for linecheck in [x.strip() for x in open("input.txt").readlines()]]] 
          if y in range(min(d,b), max(d,b))]+[abs(d-b), abs(c-a)]) for c, d in galaxy_coords[i+1:]]) for i, (a, b) in enumerate(galaxy_coords)])) """
