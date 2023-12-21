0ff897b1c3b0e2393df06cea308ad48803b628b1
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2023-12-21 21:13:23 +0000

    Day 21 part 2

--- 2023/Day 21/day21.py
+++ 2023/Day 21/day21.py
@@ -1,29 +1,42 @@
 import numpy as np
+import time
+import functools
 
 lines = open("D:/GitHub/Advent-Of-Code/2023/Day 21/input.txt").read().splitlines()
 print(lines)
+
 start = [(x,y) for x in range(len(lines[0])) for y in range(len(lines)) if lines[y][x] == "S"][0]
+x_size = len(lines[0])
+y_size = len(lines)
 
-even_steps = set([(start,0)])
-odd_steps = set([])
 
-def make_steps(full_list, empty_list):
+
+def make_steps(full_list):
+  empty_list = set()
   while full_list:
-    step_start, step_count = full_list.pop()
-    x, y = step_start
+    x, y = full_list.pop()
     for dir_x, dir_y in [(0,-1), (0,1), (1,0), (-1,0)]:
       new_x = x+dir_x
       new_y = y+dir_y
-      try:
-        if lines[new_y][new_x] == '#':
-          continue
-      except:
-        continue
-      empty_list.add(((new_x, new_y), step_count+1))
+      if find_deltas(new_x % x_size, new_y % y_size):
+        empty_list.add((new_x, new_y))
+  return empty_list
 
-for x in range(32):
-  make_steps(even_steps, odd_steps)
-  make_steps(odd_steps, even_steps)
+@functools.cache
+def find_deltas(x_coord, y_coord):
+  try:
+    if lines[y_coord][x_coord] == '#':
+      return False
+  except:
+    return False
+  return True
 
-
-print(len(set(even_steps)))
+for x in range(65, 1000, 131):
+  starttime = time.perf_counter()
+  steps = set([start])
+  print(f"{x},", end=" ")
+  for _ in range(x):
+    steps = make_steps(steps)
+  print(len(set(steps)))
+  endtime = time.perf_counter()
+  #print(f"Time: {endtime-starttime}")
