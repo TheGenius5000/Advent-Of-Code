8d42dc60ba5a9edfec59fc0bb91b7d51c5305106
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-07 15:18:28 +0000

    Day 7 cleanup

--- 2024/Day 7/day7.py
+++ 2024/Day 7/day7.py
@@ -1,4 +1,3 @@
-import itertools
 import time
 import functools
 from operator import mul, add
@@ -6,16 +5,10 @@ from operator import mul, add
 start_time = time.time()
 
 lines = {int((split_x := x.split(": "))[0]): list(map(int, split_x[1].split())) for x in open("input.txt").read().splitlines()}
-success = []
-targets = []
-total, total2 = 0,0
-
-n = 1
+total = [0,0]
 
 def concat(a, b):
   return int(str(a)+str(b))
-
-print(concat(62, 4))
 
 @functools.cache
 def combinations(length, operations):
@@ -27,36 +20,21 @@ def combinations(length, operations):
       new_set.add((operation,)+subset)
   return new_set
 
-print(combinations(2, (mul, add)))
-
 for target, nums in lines.items():
-  targets.append(target)
-  for operations in combinations(len(nums)-1,(mul,add)):
-    evaluation = nums[0]
-    for i, f in enumerate(operations):
-      evaluation = f(evaluation, nums[i+1])
-    if evaluation == target:
-      total += target
-      success.append(target)
-      break
+  for total_part, operations_part in [(0, (mul,add)), (1, (mul,add,concat))]:
+    for operations in combinations(len(nums)-1,operations_part):
+      evaluation = nums[0]
+      for i, f in enumerate(operations):
+        evaluation = f(evaluation, nums[i+1])
+      if evaluation == target:
+        total[total_part] += target
+        #success.append(target)
+        break
+    else: continue
+    break
 
-print(total)
-failed = list(set(targets) - set(success))
-
-for target in failed:
-  nums = lines[target]
-  for operations in combinations(len(nums)-1,(mul,add,concat)):
-    evaluation = nums[0]
-    for i, f in enumerate(operations):
-      evaluation = f(evaluation, nums[i+1])
-    if evaluation == target:
-      total2 += target
-      success.append(target)
-      break
-
-print(total2+total)
+print(total[0])
+print(total[1]+total[0])
 
 end_time = time.time()
-
-
 print(f"{end_time-start_time} seconds")
