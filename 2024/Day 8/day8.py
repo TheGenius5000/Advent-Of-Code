d030cc368378a0177e1c34078df93df921785327
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-08 13:16:52 +0000

    Day 8 Part 2

--- 2024/Day 8/day8.py
+++ 2024/Day 8/day8.py
@@ -19,10 +19,20 @@ for frequency, xys in antennas.items():
       for a,b in coords[i+1:]:
         anti_x = x + x-a
         anti_y = y + y-b
-        if (0 <= anti_x < len(lines[0])) and (0 <= anti_y < len(lines)): anti_coords[0].append((anti_x, anti_y))
-        if lines[anti_y][anti_x] == '.': lines[anti_y][anti_x] = '#'
+        anti_coords[1].append((x,y))
+        anti_coords[1].append((a,b))
+        if (0 <= anti_x < len(lines[0])) and (0 <= anti_y < len(lines)): 
+          anti_coords[0].append((anti_x, anti_y))
+        else: continue
+        while True:
+          if not((0 <= anti_x < len(lines[0])) and (0 <= anti_y < len(lines))): break
+          anti_coords[1].append((anti_x, anti_y))
+          if lines[anti_y][anti_x] == '.': lines[anti_y][anti_x] = '#'
+          anti_x += x-a
+          anti_y += y-b
 
 print(len(set(anti_coords[0])))
+print(len(set(anti_coords[1])))
 
 end_time = time.time()
 print(f"{end_time-start_time} seconds")
