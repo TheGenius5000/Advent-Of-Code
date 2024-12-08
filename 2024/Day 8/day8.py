2469ef32d565f7600c97fc56b42827955afe33dd
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-08 12:50:57 +0000

    Day 8 Part 2

---
+++ 2024/Day 8/day8.py
@@ -0,0 +1,28 @@
+import time
+
+start_time = time.time()
+
+lines = [list(x) for x in open("input.txt").read().splitlines()]
+
+antennas = dict()
+anti_coords = [[], []]
+
+for j, line in enumerate(lines):
+  for i, ch in enumerate(line):
+    if ch != '.':
+      if ch in antennas: antennas[ch].append((i,j))
+      else: antennas[ch]= [(i,j)]
+
+for frequency, xys in antennas.items():
+  for coords in [xys, list(reversed(xys))]:
+    for i, (x,y) in enumerate(coords):
+      for a,b in coords[i+1:]:
+        anti_x = x + x-a
+        anti_y = y + y-b
+        if (0 <= anti_x < len(lines[0])) and (0 <= anti_y < len(lines)): anti_coords[0].append((anti_x, anti_y))
+        if lines[anti_y][anti_x] == '.': lines[anti_y][anti_x] = '#'
+
+print(len(set(anti_coords[0])))
+
+end_time = time.time()
+print(f"{end_time-start_time} seconds")
