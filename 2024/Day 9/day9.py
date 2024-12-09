6aeb7f5205be24f02b595220305531a84282ec20
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-09 18:22:07 +0000

    Code cleanup

--- 2024/Day 9/day9.py
+++ 2024/Day 9/day9.py
@@ -3,27 +3,20 @@ import time
 
 start_time = time.time()
 
-encoding = "14113"
+#encoding = "14113"
 #encoding = "2333133121414131402" 
-encoding = open("D:\\GitHub\\Advent-Of-Code\\2024\\Day 9\\input.txt").read().strip("\n")
+encoding = open("input.txt").read().strip("\n")
 
 encoding = list(map(int, encoding))
 id = -1
 memory_block = ['.' if i%2 == 1 else str(i//2) for (i, n) in enumerate(encoding) for _ in range(n)]
 memory_block2 = [(x, len(list((y)))) for x, y in itertools.groupby(memory_block.copy())]
-encoded_memory_block = ""
-checksum = 0
 header = 0
-total2 = 0
-total1 = 0
-
-#print(encoding)z
+checksum = 0
 
 if any([x[0] == '.' for x in memory_block]):
   for i, mem in enumerate(memory_block[::-1]):
-    if memory_block.index('.') > len(memory_block)-i-1:
-      #memory_block = memory_block[:len(memory_block)-i+1]
-      break
+    if memory_block.index('.') > len(memory_block)-i-1: break
     if mem == '.': continue
     memory_block[memory_block.index('.')], memory_block[len(memory_block)-i-1] = mem, '.'
 
@@ -49,14 +42,13 @@ for mem, size in memory_block2:
     header += size
     continue
   for n in range(size):
-    total2 += int(mem)*header
+    checksum += int(mem)*header
     header += 1
 
 header = 0
 
 print(sum([int(ch)*i for i, ch in enumerate(filter(lambda x: x != '.', memory_block))]))
-print(total2)
-print(total1)
+print(checksum)
 pass
 
 end_time = time.time()
