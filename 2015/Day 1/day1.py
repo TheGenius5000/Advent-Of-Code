753c0a6deb112f18bdb121133374a8a1bb328637
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-09 19:07:19 +0000

    2015 Day 1

---
+++ 2015/Day 1/day1.py
@@ -0,0 +1,25 @@
+# Very Easy
+
+import time
+
+start_time = time.time()
+
+line = open("D:\\GitHub\\Advent-Of-Code\\2015\\Day 1\\input.txt").read()
+
+floor = 0
+basement_pos = 0
+
+for i, ch in enumerate(line):
+  if ch == '(':
+    floor += 1
+  elif ch == ')':
+    floor -= 1
+  if floor == -1 and basement_pos == 0:
+    basement_pos = i+1
+
+print(floor)
+print(basement_pos)
+
+end_time = time.time()
+
+print(f"{end_time-start_time} seconds")
