26e0ed63eb6fd575941aab376b91dfe99a975134
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-10 11:17:40 +0000

    Day 10 code cleanup

--- 2024/Day 10/day10.py
+++ 2024/Day 10/day10.py
@@ -2,13 +2,10 @@ import time
 
 start_time = time.time()
 
-lines = [list(map(int, x)) for x in open("D:\\GitHub\\Advent-Of-Code\\2024\\Day 10\\input.txt").read().splitlines()]
+lines = [list(map(int, x)) for x in open("input.txt").read().splitlines()]
 
-dirs = {"U": [0,-1], "D": [0,1], "L": [-1,0], "R":[1,0]}
-visited = set()
 visited_9 = set()
 stack = []
-#State = ((x,y), dir)
 score1 = 0
 score2 = 0
 
@@ -23,7 +20,7 @@ while stack:
       score1 += 1
       visited_9.add((x,y))
     continue
-  for dx, dy in dirs.values():
+  for dx, dy in [[0,-1],[0,1],[-1,0],[1,0]]:
     new_x, new_y = x+dx, y+dy
     if not(all([0 <= new_x < len(lines[0]), 0 <= new_y < len(lines)])): continue
     if lines[new_y][new_x] != lines[y][x] + 1: continue
