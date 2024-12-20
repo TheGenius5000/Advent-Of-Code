271e4b12bd382eabdd5e7d44010deb4764fbae2d
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2024-12-20 13:43:11 +0000

    Day 20 Part 2 (both parts optimised

--- 2024/Day 20/day20.py
+++ 2024/Day 20/day20.py
@@ -1,16 +1,27 @@
 import time
 import heapq
 
+## Problem:  A race on a 2d grid where you are given one cheat to ignore boundaries for 2 squares.
+##           Part 1: Count the number of cheats that save at least 100 distance.
+##           Part 2: You can use the one cheat to ignore boundaries for up to 20 squares.
+##           Count the number of cheats that save at least 100 distance.
+
+## Solution: Part 1: First, Dijkstra's algorithm to get all the distances for the reachable squares ('.' or 'S' or 'E') from start ('S'). 
+##           Then, Flood-fill from the start of the cheat position towards any reachable end 2 squares away, ignoring boundaries.
+##           Part 2: This again but for squares up to 20 squares away.
+
+## Average runtime: ~5.6 seconds
+
 start_time = time.perf_counter()
 
 grid = list(map(list, open(r"input.txt").read().splitlines()))
 
 ans1 = 0
+ans2 = 0
 
 dirs = [(-1,0), (1,0), (0,-1), (0,1)]
 
 start_x, start_y = (find_the_char := lambda match: [(x,y) for y, line in enumerate(grid) for x, ch in enumerate(line) if ch in match][0])('S')
-end_x, end_y = find_the_char('E')
 
 adj_matrix = [[float('inf') for _ in line] for line in grid]
 adj_matrix[start_y][start_x] = 0
@@ -28,34 +39,29 @@ while queue:
     adj_matrix[new_y][new_x] = new_distance
     heapq.heappush(queue, (new_distance, (new_x, new_y)))
 
-
-## Find all skips
+## Flood-fill
 for y, (line, adjacancy_line) in enumerate(zip(grid, adj_matrix)):
   for x, (location, distance) in enumerate(zip(line, adjacancy_line)):
-    if distance == float('inf'): continue
-    for dx, dy in dirs:
-      new_distance = distance
-      new_x, new_y = x+dx, y+dy
-      if grid[new_y][new_x] in '.SE': continue
-      new_x, new_y = new_x+dx, new_y+dy
-      if not all([0 <= new_x < len(line), 0 <= new_y < len(grid)]): continue
-      if grid[new_y][new_x] == '#': continue
-      new_distance += 2
-      if not(new_distance < adj_matrix[new_y][new_x]) or adj_matrix[new_y][new_x] == float('inf'): continue
-      distance_saved = adj_matrix[new_y][new_x] - new_distance
-      cheats[((x,y),(new_x,new_y))] = distance_saved
-      # if distance_saved not in cheats: cheats[distance_saved] = set([((x,y),(new_x, new_y))])
-      # else: cheats[distance_saved].add(((x,y),(new_x, new_y)))
+    if location == '#': continue
+    start = (x,y)
+    for delta_x in range(-20,21):
+      for delta_y in range(-20,21):
+        if (delta_distance := abs(delta_x) + abs(delta_y)) > 20: continue
+        new_x, new_y = x+delta_x, y+delta_y
+        if not all([0 <= new_x < len(line), 0 <= new_y < len(grid)]): continue
+        if grid[new_y][new_x] == '#': continue
+        new_distance = distance + delta_distance
+        if not(new_distance < adj_matrix[new_y][new_x]) or adj_matrix[new_y][new_x] == float('inf'): continue
+        distance_saved = adj_matrix[new_y][new_x] - new_distance
+        cheats[(start, (new_x, new_y))] = distance_saved
 
-for cheat_skip, time_saved in cheats.items():
+for ((from_x, from_y), (to_x, to_y)), time_saved in cheats.items():
   if time_saved < 100: continue
-  ans1 += 1
-
-# for time_saved, cheat_skips in cheats.items():
-#   if len(cheat_skips) < 100: continue
-#   ans1 += len(cheat_skips)
+  ans2 += 1
+  if abs(from_x - to_x) + abs(from_y - to_y) == 2: ans1 += 1
 
 print(ans1)
+print(ans2)
 
 end_time = time.perf_counter()
 
