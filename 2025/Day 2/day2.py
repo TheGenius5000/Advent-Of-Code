74f820f0ab58499db4ffb1fbf364c9c089063b08
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2025-12-02 15:55:45 +0000

    Day 2 Part 2

--- 2025/Day 2/day2.py
+++ 2025/Day 2/day2.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 ## Day 2 - Gift Shop
 ## Average runtime: ~
@@ -39,20 +40,67 @@ for start, end in id_ranges:
   end = int(end)
   if check_if_repeating(start): 
     ans1 += start
-    print(start)
+    #print(start)
   num = start
   while num < end:
     num = get_next_repeating_number(num)
     if start <= num <= end: 
-      print(num)
+      #print(num)
       ans1 += num
 
-print(f"The sum of invalid IDs is {ans1}")
+print(f"The sum of invalid IDs for part 1 is {ans1}")
 
 ## Part 2
-# Problem: 
+# Problem: Find the sum of invalid IDs 
 
-# Solution: 
+# Solution: Go through the range of IDs, finding each repeating number. The function has been generalised to handle any n-length sequence of repeating numbers.
+
+#N - how many repeating digits there are.
+def get_next_n_repeating_number(num, n):
+  str_num = str(num)
+  multiple = len(str_num)//n
+  repeating_num = int(str_num[:n])
+  new_num = int(str(repeating_num)*multiple)
+  if new_num <= num:
+    if str(repeating_num) == "9"*n: 
+      repeating_num = "1"+"0"*(n-1)
+      new_num = int(repeating_num*(multiple+1))
+    else: 
+      repeating_num += 1
+      new_num = int(str(repeating_num)*multiple)
+  return new_num
+
+def check_if_n_repeating_number(num, n):
+  if num < 10: return False
+  str_num = str(num)
+  repeating_num = str_num[:n]
+  return int(repeating_num*(len(str_num)//n)) == num
+
+#Sum of all invalid IDs (for part 2)
+ans2 = 0
+
+for start, end in id_ranges:
+  str_start, str_end = start, end
+  start = int(start)
+  end = int(end)
+  invalid_ids = set()
+  invalid_idsp1 = set()
+  for n in range(1, len(str_end)//2+1):
+    if (len(str_start) % n == 0) or (len(str_end) % n == 0):
+      num = start
+    else:
+      continue 
+    if check_if_n_repeating_number(start, n): invalid_ids.add(start)
+
+    while num < end:
+      num = get_next_n_repeating_number(num, n)
+      if num < 10: continue
+      if num == -1: break
+      if start <= num <= end:
+        invalid_ids.add(num)
+  ans2 += sum(invalid_ids)
+
+print(f"The sum of all invalid IDs for part 2 is {ans2}")
 
 end_time = time.perf_counter()
 
