490adb3b8996f292f0fcf4547ca6a37a7b49d6a5
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2025-12-08 18:44:24 +0000

    Day 8 Part 2

--- 2025/Day 8/day8.py
+++ 2025/Day 8/day8.py
@@ -1,12 +1,13 @@
 import time
 import math
+import copy
 
 ## Day 8 - Playground
-## Average runtime: ~
+## Average runtime: ~2.05 seconds
 
 start_time = time.perf_counter()
 
-with open(r"D:\GitHub\Advent-Of-Code\2025\Day 8\input.txt") as f:
+with open(r"input.txt") as f:
   lines = [list(map(int, line.split(","))) for line in f.read().splitlines()]
 
 
@@ -20,13 +21,13 @@ possible_connections = sorted({(math.dist(coords0, coords1), (tuple(coords0), tu
 
 circuits = [{tuple(x)} for x in lines]
 
-for _ in range(1000):
+iterations = 0
+
+while len(circuits) > 1:
+  if iterations == 1000: ans1_circuits = copy.deepcopy(circuits)
   shortest_connection = possible_connections.pop(0)
   dist, (coords0, coords1) = shortest_connection
-  for i, circuit in enumerate(circuits):
-    if coords0 in circuit:
-      current_circuit = circuits.pop(i)
-      break
+  current_circuit = circuits.pop(next(i for i, circuit in enumerate(circuits) if coords0 in circuit))
   for i, circuit in enumerate(circuits):
     if coords1 in circuit:
       merge_circuit = circuits.pop(i)
@@ -35,21 +36,20 @@ for _ in range(1000):
     merge_circuit = set()
   current_circuit |= merge_circuit
   circuits.append(current_circuit)
+  iterations += 1
 
-circuits = sorted(circuits, key=len)
-circuits.reverse()
 
-for circuit in circuits:
-  print(circuit)
-
-ans1 = math.prod(map(len, circuits[:3]))
+ans1_circuits = sorted(ans1_circuits, key=len, reverse=True)[:3]
+ans1 = math.prod(map(len, ans1_circuits[:3]))
+ans2 = coords0[0]*coords1[0]
 
 print(f"The product of the sizes of the 3 largest circuits are {ans1}.")
+print(f"The product of the final circuit-merging connection is {ans2}.")
 
 ## Part 2
-# Problem: 
+# Problem: What is the multiplication of the X coordinates for the first two boxes that connect to form one single circuit for all boxes?
 
-# Solution: 
+# Solution: Modify the above solution to carry on until the list of circuits is 1. You have the last connection left over from the loop, so times the X coords together.
 
 
 end_time = time.perf_counter()
