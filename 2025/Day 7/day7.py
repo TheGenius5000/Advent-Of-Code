536e7680538cb9d82813dce2da6456e902f489bc
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2025-12-13 14:40:17 +0000

    Day 7 Part 2

--- 2025/Day 7/day7.py
+++ 2025/Day 7/day7.py
@@ -1,11 +1,11 @@
 import time
 
 ## Day 7 - Laboratories
-## Average runtime: ~
+## Average runtime: ~0.0035 seconds
 
 start_time = time.perf_counter()
 
-with open(r"D:\GitHub\Advent-Of-Code\2025\Day 7\input_test.txt") as f:
+with open(r"input.txt") as f:
   lines = f.read().splitlines()
 
 ## Part 1
@@ -30,13 +30,26 @@ print(f"The number of tachyon beam splits is {splits}.")
 ## Part 2
 # Problem: Figure out many paths the beam could've took, and give the total of that.
 
-# Solution: Do the splits again, but keep track of how many paths it took to reach a certain splitter.
+# Solution: Do the splits again, but keep track of how many paths it took to reach a certain splitter. This is done via a hashmap.
 
-# beams = {(lines[0].index("S"), 1)}
+beams = {lines[0].index("S"): 1}
 
-# for line in lines[1:]:
-#   for i, ch in enumerate(line):
+j = 1
+while j < len(lines):
+  new_beams = {k: v for k, v in beams.items()}
+  for beam, beam_count in sorted(beams.items(), key=lambda x: x[0]):
+    if lines[j][beam] != "^": continue
+    if beam-1 in new_beams:  new_beams[beam-1] += beam_count
+    else: new_beams[beam-1] = beam_count
+    if beam+1 in new_beams:  new_beams[beam+1] += beam_count
+    else: new_beams[beam+1] = beam_count
+    del new_beams[beam]
+  j += 1
+  beams = new_beams
 
+ans2 = sum(beams.values())
+
+print(f"The number of possible timelines is {ans2}.")
 
 end_time = time.perf_counter()
 
