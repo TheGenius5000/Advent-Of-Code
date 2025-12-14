335407a0c305042464b02f4f45109b5e94445e71
Ganesh Bargota <117196974+TheGenius5000@users.noreply.github.com>
2025-12-14 05:46:28 +0000

    Day 11 Part 2

--- 2025/Day 11/day11.py
+++ 2025/Day 11/day11.py
@@ -1,4 +1,5 @@
 import time
+import functools
 
 ## Day 11 - Reactor
 ## Average runtime: ~
@@ -34,6 +35,28 @@ print(f"The number of routes to 'out' is {ans1}.")
 # Problem: 
 
 # Solution: 
+
+ans2 = 0
+stack = [("svr", False, False, 0)]
+good_routes = set()
+bad_routes = set()
+route_history = {0: []}
+
+@functools.cache
+def connection_travel(cable, fft_visit, dac_visit):
+  if (cable == "out") and all([fft_visit, dac_visit]): return 1
+  elif cable == "out": return 0
+  if cable == "fft": fft_visit = True
+  if cable == "dac": dac_visit = True
+
+  total = 0
+  for sub_cable in cables[cable]:
+    total += connection_travel(sub_cable, fft_visit, dac_visit)
+  return total
+
+ans2 = connection_travel("svr", False, False)
+
+print(f"The number of routes visiting 'fft' and 'dac' is {ans2}.")
 
 
 end_time = time.perf_counter()
